318ed500ed17d96c946d09b6e9bbbc0bcb115dae
Crauzer <filip.quitko@gmail.com>
2024-07-03 13:54:19 +0200

    feat: add VfxConverter

---
+++ lol-convert/Converters/VfxConverter.cs
@@ -0,0 +1,20 @@
+﻿using LeagueToolkit.Meta;
+using lol_convert.Wad;
+
+namespace lol_convert.Converters;
+
+internal class VfxConverter
+{
+    private readonly WadHashtable _wadHashtable;
+    private readonly MetaEnvironment _metaEnvironment;
+    private readonly string _outputPath;
+
+    public VfxConverter(WadHashtable wadHashtable,
+        MetaEnvironment metaEnvironment,
+        string outputPath)
+    {
+        this._wadHashtable = wadHashtable;
+        this._metaEnvironment = metaEnvironment;
+        this._outputPath = outputPath;
+    }
+}
