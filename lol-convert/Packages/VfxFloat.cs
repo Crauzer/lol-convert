ba72f4c8fe240df7bf6e4a33f0ce1507a6ea848e
Crauzer <filip.quitko@gmail.com>
2024-07-03 15:59:40 +0200

    feat: add vfx float

---
+++ lol-convert/Packages/VfxFloat.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+using MetaClass = LeagueToolkit.Meta.Classes;
+
+namespace lol_convert.Packages;
+
+internal class VfxFloat(MetaClass.ValueFloat valueFloat)
+{
+    public float ConstantValue { get; set; } = valueFloat.ConstantValue;
+    public VfxFloatDynamicsBase Dynamics { get; set; } =
+        valueFloat.Dynamics switch
+        {
+            MetaClass.VfxAnimatedFloat dynamics => new VfxFloatDynamics(dynamics),
+            MetaClass.VfxAnimatedFloatVariableData dynamics
+                => new VfxFloatVariableDynamics(dynamics),
+            _ => throw new InvalidOperationException("Unknown Float dynamics")
+        };
+}
+
+[JsonDerivedType(typeof(VfxFloatDynamics), typeDiscriminator: "static")]
+[JsonDerivedType(typeof(VfxFloatVariableDynamics), typeDiscriminator: "variable")]
+internal class VfxFloatDynamicsBase { }
+
+internal class VfxFloatDynamics(MetaClass.VfxAnimatedFloat dynamics) : VfxFloatDynamicsBase
+{
+    public List<byte> Modes { get; set; } = [.. dynamics.Modes];
+    public List<float> Values { get; set; } = [.. dynamics.Values];
+    public List<float> Times { get; set; } = [.. dynamics.Times];
+}
+
+internal class VfxFloatVariableDynamics(MetaClass.VfxAnimatedFloatVariableData dynamics)
+    : VfxFloatDynamicsBase
+{
+    public List<float> Values { get; set; } = [.. dynamics.Values];
+    public List<float> Times { get; set; } = [.. dynamics.Times];
+    public List<VfxProbabilityTable> ProbabilityTables { get; set; } =
+        dynamics
+            .ProbabilityTables?.Select(probabilityTable => new VfxProbabilityTable(
+                probabilityTable
+            ))
+            .ToList() ?? [];
+}
