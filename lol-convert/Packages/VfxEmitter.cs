1c321044c6480c9c7f642871b4701b9882558381
Crauzer <filip.quitko@gmail.com>
2024-07-18 16:32:54 +0200

    feat: serialize material override

--- lol-convert/Packages/VfxEmitter.cs
+++ lol-convert/Packages/VfxEmitter.cs
@@ -280,8 +280,6 @@ internal class VfxLegacySimple(MetaClass.VfxEmitterLegacySimple legacySimple) { 
 
 internal class VfxLinger(MetaClass.VfxLingerDefinitionData linger) { }
 
-internal class VfxMaterialOverride(MetaClass.VfxMaterialOverrideDefinitionData materialOverride) { }
-
 internal class VfxTextureMult(MetaClass.VfxTextureMultDefinitionData textureMult) { }
 
 internal class VfxReflection(MetaClass.VfxReflectionDefinitionData reflection)
